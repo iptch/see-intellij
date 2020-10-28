1cc72d4ec456a5c1063e844db83fdeac6ea8464b
Jennifer Studer <jst@ipt.ch>
2020-10-28 18:56:55 +0100

    add attribute to team

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -3,6 +3,16 @@ import java.util.List;
 public class Team {
 
     private List<Footballer> fotballers;
+    private String coach;
+
+    public Team(String coach) {
+        this.coach = coach;
+    }
+
+    public Team(List<Footballer> fotballers, String coach) {
+        this.fotballers = fotballers;
+        this.coach = coach;
+    }
 
     public List<Footballer> getFotballers() {
         return fotballers;
@@ -10,5 +20,13 @@ public class Team {
 
     public void setFotballers(List<Footballer> fotballers) {
         this.fotballers = fotballers;
+    }
+
+    public String getCoach() {
+        return coach;
+    }
+
+    public void setCoach(String coach) {
+        this.coach = coach;
     }
 }
